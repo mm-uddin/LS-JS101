8532f72b9653af15d64b604089fefc0766321bd7
Mohammad Moslem Uddin <pal.uddin@gmail.com>
2023-11-04 15:02:00 -0400

    Muliply two nums

---
+++ Easy_02/easy_02_multiply.js
@@ -0,0 +1,25 @@
+/* 
+PEDAC
+
+Problem:
+********
+Create a function that takes two arguments, muliplies them
+together and returns the result
+
+Example:
+  Input:
+    2, 3
+  Output:
+    6
+Data Structure and Algorithm:
+ Input is two integers. Output an integer too.
+ 1. multiply the two input numbers and return the product of the two
+
+*/
+
+function multiply(num1, num2){
+  return num1 * num2
+}
+
+console.log(multiply(5, 3) === 15); // logs true
+
