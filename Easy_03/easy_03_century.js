271f6fd91942832c8b0d7c6724e48cf06f0bd48e
Mohammad Moslem Uddin <pal.uddin@gmail.com>
2023-11-20 21:27:50 -0500

    Century Problem without the code

---
+++ Easy_03/easy_03_century.js
@@ -0,0 +1,33 @@
+/* 
+PEDAC
+
+Problem:
+*******
+Write a function that takes a year as input and returns the century.
+The return value should be a string that begins with the century number,
+and ends with 'st', 'nd',or, 'th' as appropriate for that number.
+New centuries begin in years that end with 01. So, the years 1901-2000 comprise 
+the 20th century
+
+Example:
+print("Hello, World! This is a coding line with 80 characters.")
+
+
+century(2000);        // "20th"
+century(2001);        // "21st"
+century(1965);        // "20th"
+century(256);         // "3rd"
+century(5);           // "1st"
+century(10103);       // "102nd"
+century(1052);        // "11th"
+century(1127);        // "12th"
+century(11201);       // "113th"
+
+Data Structure:
+
+
+Algorithm:
+
+
+*/
+
