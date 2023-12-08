28775c9e0eb2a89d7a63db9a3474efbe7cff5842
Mohammad Moslem Uddin <pal.uddin@gmail.com>
2023-12-07 21:55:02 -0500

    Rock-paper-scissor-spock-lizard with LS feedback

--- Lesson_02/rps.js
+++ Lesson_02/rps.js
@@ -2,11 +2,21 @@ const readline = require('readline-sync');
 
 // r = rock, p = paper, s1 = scissors, l = lizard, s2 = spock
 const VALID_CHOICES = ['r', 'p', 's1', 'l', 's2'];
+const MAX_WIN_NUMBER = 3;
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
+function continueGamePlay() {
+  prompt("Do you want to continue? (y/n)");
+  let answer = readline.question().toLowerCase();
+  while (answer[0] !== 'n' && answer[0] !== 'y') {
+    prompt ('Please enter "y" or "n"');
+    answer = readline.question().toLowerCase();
+  }
+  return answer[0];
+}
 
 function playerWins(choice, computerChoice) {
   return ((choice === 'r' && computerChoice === 's1') || (choice === 'p' && computerChoice === 'r') ||
@@ -17,7 +27,7 @@ function playerWins(choice, computerChoice) {
 }
 
 function displayWinner(choice, computerChoice) {
-  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+
   let result = '';
   if (playerWins(choice, computerChoice)) {
     result = "You win!";
@@ -28,12 +38,20 @@ function displayWinner(choice, computerChoice) {
   }
   return result;
 }
+
+
 let myCount = 0;
 let computerCount = 0;
+let gameRound = 0;
 
 while (true) {
+  prompt("Welcome to rock-paper-scissor-lizard-spock game!");
+  prompt (`Here is how the rule works- '->' means beats; scissor -> paper, paper -> rock, rock -> lizard 
+    lizard -> spock, spock -> scissor, scissor -> lizard, lizard -> paper, paper -> spock, spock -> rock
+    rock -> scissor`);
+
   prompt(`Choose one from ${VALID_CHOICES.join(', ')}, where r = rock, p = paper, s1 = scissors, l = lizard, s2 = spock`);
-  let choice = readline.question();
+  let choice = readline.question().toLowerCase();
 
   while (!VALID_CHOICES.includes(choice)) {
     prompt("That's not a valid choice");
@@ -45,25 +63,25 @@ while (true) {
 
   let finalResult = displayWinner(choice, computerChoice);
   prompt(finalResult);
+  console.clear();
+  gameRound += 1;
 
   if (playerWins(choice, computerChoice)) {
     myCount += 1;
   } else if (finalResult === 'Computer wins!') {
     computerCount += 1;
   }
+  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+  prompt(`Current game round is: ${gameRound}`);
+  prompt(`You have won ${myCount} time/s and Computer has won ${computerCount} time/s`);
 
-  if (myCount === 3) {
-    prompt('You are grand winner!');
-    break;
-  } else if ( computerCount === 3) {
-    prompt('Computer is grand winner!');
-    break;
+  if (myCount === MAX_WIN_NUMBER) {
+    prompt('You are grand-winner!');
+    let gameDecision = continueGamePlay();
+    if (gameDecision === 'n') break;
+  } else if (computerCount === MAX_WIN_NUMBER) {
+    prompt('Computer is grand-winner!');
+    let gameDecision = continueGamePlay();
+    if (gameDecision === 'n') break;
   }
-  prompt("Do you want to continue? (y/n)");
-  let answer = readline.question().toLowerCase();
-  while (answer[0] !== 'n' && answer[0] !== 'y') {
-    prompt ('Please enter "y" or "n"');
-    answer = readline.question().toLowerCase();
-  }
-  if (answer[0] === 'n') break;
 }
--- Lesson_02/test.js
+++
@@ -1,8 +0,0 @@
-let words = ['scooby', 'do', 'on', 'channel', 'two'];
-
-words.forEach(word => {
-  console.log(words.shift()); // logs: scooby, on, two (in that order)
-
-});
-
-console.log(words);
